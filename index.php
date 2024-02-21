f267f8e023ab4d774a500aea99ff7c1536fe6be4
Pablo <115142181+Readypablo@users.noreply.github.com>
2024-02-21 12:05:16 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    
+<header>
+
+
+<a href="index.php" class="link_log"><p>Нарушениям нет</p></a>
+</header>
+
+<main>
+
+
+<div class="menuhad">
+
+<a href="pages/login.php" class="link_head">Вход</a>
+
+<a href="pages/registration.php" class="link_head">регистрации</a>
+<a href="pages/profil.php" class="link_head">Профиль</a>
+<a href="pages/catalog.php" class="link_head">формирования заявления</a>
+
+</div>
+
+
+</div>
+
+
+</div>
+</main>
+
+
+</body>
+</html>
